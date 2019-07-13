bc2231daa95f7563ba46a1c8f788ee32e26fb325
szoroh <kubahorosz95@gmail.com>
2019-07-13 16:32:38 +0200

    Add city to trip object

--- app/forms/trip_form.rb
+++ app/forms/trip_form.rb
@@ -1,10 +1,11 @@
 class TripForm
   include ActiveModel::Model
 
-  attr_accessor :note, :user_id
+  attr_accessor :note, :user_id, :city_name
 
   validates :note, presence: true
   validates :user_id, presence: true
+  validates :city_name, presence: true
 
   def save
     return false unless valid?
@@ -17,7 +18,12 @@ class TripForm
   def trip
     @trip = Trip.new(
       note: note,
-      user_id: user_id
+      user_id: user_id,
+      city: city
     )
+  end
+
+  def city
+    @city = City.find_or_create_by(name: city_name)
   end
 end
