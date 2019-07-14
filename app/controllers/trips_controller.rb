5bf1828b4adcd23d7e3f37ab8dcee8c5affd61a2
szoroh <kubahorosz95@gmail.com>
2019-07-14 17:55:25 +0200

    Refactor views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,12 +9,34 @@ class TripsController < ApplicationController
     @trips = Trip.all
   end
 
+  def edit
+    @trip = Trip.find(params[:id])
+
+  end
+
+  def update
+    #TODO edit from index view
+    @trip = Trip.find(params[:id])
+
+    if @trip.update(trip_params)
+      redirect_to @trip
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to trips_path, notice: 'Trip successfully destroyed'
+  end
+
   def create
     weather_object = WeatherObtainer.new(params[:trip_form][:city_name]).call
     @trip_form = TripForm.new(trip_params.merge(temperature: weather_object))
 
     if @trip_form.save
-      redirect_to root_path, notice: 'Trip successfully created'
+      redirect_to trips_path, notice: 'Trip successfully created'
     else
       render :new
     end
