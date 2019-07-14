e5427aea7bcd6a76b8b64803f032c2de67743d2d
szoroh <kubahorosz95@gmail.com>
2019-07-14 03:42:20 +0200

    Change temperature from float to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_195643) do
+ActiveRecord::Schema.define(version: 2019_07_14_011122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_07_13_195643) do
   end
 
   create_table "weathers", force: :cascade do |t|
-    t.float "temperature"
+    t.integer "temperature"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
